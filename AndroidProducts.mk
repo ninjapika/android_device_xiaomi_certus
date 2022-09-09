d46604357d7b0c0674f4abd8e79b3a1479e8a889
Ninja Pika <76650191+ninjapika@users.noreply.github.com>
2022-09-09 12:53:45 +0530

    Sparkify!
    Signed-off-by: Ninja Pika <76650191+ninjapika@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_certus.mk
+    $(LOCAL_DIR)/spark_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_certus-user \
-    nad_certus-userdebug \
-    nad_certus-eng
+    spark_certus-user \
+    spark_certus-userdebug \
+    spark_certus-eng
